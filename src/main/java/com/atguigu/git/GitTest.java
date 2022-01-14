eda3b8c0b08f5f35b8c9b1f880da95b9108e4a14
chenhu <chenhu@qq.com>
2022-01-14 14:30:47 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 
 }
